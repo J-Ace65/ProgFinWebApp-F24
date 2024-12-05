47e285d087893db22af8ea46f6e2a53badab8258
J-Ace65 <jeffacevedo65@gmail.com>
2024-12-04 20:36:00 -0500

    minor fix

--- confiq/db.js
+++ confiq/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '02051218Ejc!!',
-    database: 'project_pt3',
+    password: 'root',
+    database: 'root',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
